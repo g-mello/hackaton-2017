a0702b58705972fb821f1611b1cb3b0672bc2ea6
guilhermepeixoto <guilherme_peixoto_@hotmail.com>
2017-10-16 00:15:12 -0200

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,29 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http) {
+  
+  
+//-------------------------------------------------------------------------------------------------------------
+
+        //Codigo para API do CEP
+
+ //-------------------------------------------------------------------------------------------------------------
+
+  $scope.consultarEndereco = function (cep){
+    var urlApi = 'https://viacep.com.br/ws/'+cep+'/json/';
+    console.log(urlApi)
+    $http.get(urlApi)
+    .success(function (data){   
+      console.log(data) 
+        $scope.endereco = data; 
+    })
+
+    .error(function (data, status, headers, config) {
+      alert('Sorry, no service available!')
+    });
+  }
+});
+
+
+
+
