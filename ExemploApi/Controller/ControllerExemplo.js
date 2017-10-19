d11668a4174a089ab774b9bf5223d9dafcc2fd13
Pedro Figueredo <pedrowiller25@gmail.com>
2017-10-19 13:19:38 -0200

    Alterado arquivos da api

--- ExemploApi/Controller/ControllerExemplo.js
+++ ExemploApi/Controller/ControllerExemplo.js
@@ -2,6 +2,7 @@ var exemplo = require('../Repository/ExemploRepository.js')
 
 var exemploRota = function(app) {
     app.get('/Get', exemplo.ex);
+    app.get('/GetCidadao', exemplo.BuscaCidadao);
 }
 
 module.exports = exemploRota;
--- ExemploApi/Repository/ExemploRepository.js
+++ ExemploApi/Repository/ExemploRepository.js
@@ -8,6 +8,21 @@ var exemplo = {
                 // Parametros de entrada e saida
                 // .input('input_parameter', sql.Int, value)
                 //.output('output_parameter', sql.VarChar(50))
+                .execute('sp_teste', (err, result) => {
+                    sql.close();
+                    return res.json(result.recordset);
+                })
+        })
+        sql.on('error', err => {
+            return res.json("Erro ao conectar com banco de dados" + err);
+        })
+    },
+    BuscaCidadao: (req, res) => {
+        sql.connect(config.conString, err => {
+            new sql.Request()
+                // Parametros de entrada e saida
+                // .input('input_parameter', sql.Int, value)
+                //.output('output_parameter', sql.VarChar(50))
                 .execute('sp_SelCidadao', (err, result) => {
                     sql.close();
                     return res.json(result.recordset);
@@ -16,7 +31,6 @@ var exemplo = {
         sql.on('error', err => {
             return res.json("Erro ao conectar com banco de dados" + err);
         })
-        
     }
 }
 
