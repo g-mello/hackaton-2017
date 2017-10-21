bd3d7caa770113bdc69396c0f01ac04c85861263
guilhermepeixoto <guilherme_peixoto_@hotmail.com>
2017-10-21 11:07:03 -0200

    Ajustando arquivos sql procedures e query

--- Script SQL/proceduresCO.sql
+++ Script SQL/proceduresCO.sql
@@ -1,24 +1,11 @@
-/*
+DROP PROCEDURE SP_InsCidadao
+
 CREATE PROCEDURE sp_SelCidadao
 AS
 SELECT * 
 FROM tb_cidadao
 GO
 
-
-CREATE PROCEDURE sp_InsCidadao
-    @nome VARCHAR(60),
-    @sobrenome VARCHAR(60),
-    @rg VARCHAR(12),
-    @cpf VARCHAR(11),
-    @telefone VARCHAR(12),
-    @email VARCHAR(80)
-AS
-BEGIN
-INSERT INTO tb_cidadao( nome, sobrenome, rg, cpf, telefone, email) VALUES
-(@nome, @sobrenome, @rg, cpf, @telefone, @email) 
-END
-*/
 
 CREATE PROCEDURE sp_InsRequerimento
     @id_cidadao INTEGER,
@@ -34,6 +21,8 @@ CREATE PROCEDURE sp_InsRequerimento
     @uf CHAR(2),
     @ponto_referencia VARCHAR(100),
     @observacao VARCHAR(MAX),
+    @caminho VARCHAR(120),
+    @data_envio DATETIME,
     @status_req INTEGER
 AS
 BEGIN
@@ -51,7 +40,9 @@ INSERT INTO tb_requerimento(
                         uf,
                         ponto_referencia,
                         observacao,
-                        status_req
+                        caminho,
+                        data_envio,
+                        status_req                        
 ) VALUES
 (
     @id_cidadao,
@@ -67,8 +58,32 @@ INSERT INTO tb_requerimento(
     @uf,
     @ponto_referencia,
     @observacao,
+    @caminho,
+    @data_envio,
     @status_req
 )
+SELECT SCOPE_IDENTITY()
 END
+GO
 
 
+CREATE PROCEDURE SP_InsCidadao
+	@nome       VARCHAR(60),
+	@sobrenome	VARCHAR(60),
+	@rg			VARCHAR(12),
+	@cpf		VARCHAR(11),
+	@telefone	VARCHAR(12),
+	@email		VARCHAR(80)
+AS 
+	BEGIN
+		BEGIN TRANSACTION
+		INSERT INTO [dbo].[tb_cidadao] (nome, sobrenome, rg, cpf, telefone, email)
+			VALUES(@nome, @sobrenome, @rg, @cpf, @telefone, @email)
+		SELECT  SCOPE_IDENTITY() AS 'id_cidadao'
+		COMMIT TRANSACTION
+	END
+GO
+
+declare @x int 
+EXEC @x = SP_InsCidadao 'no','no','no','no','no','no'
+SELECT @x
--- Script SQL/query.sql
+++ Script SQL/query.sql
@@ -5,7 +5,7 @@ IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[t
 	DROP TABLE [dbo].[tb_cidadao]
 
 CREATE TABLE [dbo].[tb_cidadao] (
-  id_cidadao INTEGER NOT NULL IDENTITY(1,1),
+  id_cidadao INTEGER    NOT NULL IDENTITY(1,1),
   nome      VARCHAR(60) NOT NULL,
   sobrenome VARCHAR(60) NOT NULL,
   rg        VARCHAR(12) NOT NULL,
@@ -20,33 +20,28 @@ IF EXISTS(SELECT TOP 1 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[t
 	DROP TABLE [dbo].[tb_requerimento]
 
 CREATE TABLE [dbo].[tb_requerimento] (
-  id_requerimeto    INTEGER NOT NULL IDENTITY(1,1),
-  id_cidadao        INTEGER NOT NULL,
-  cod_controle      VARCHAR(10) NOT NULL,
-  latitude          VARCHAR(50) NULL,
-  longitude         VARCHAR(50) NULL,
-  cep               INTEGER NULL,
-  logradouro        VARCHAR(100) NOT NULL,
-  numero            INTEGER NULL, 
-  bairro            VARCHAR(60) NOT NULL,
-  complemento       VARCHAR(60) NULL,
-  cidade            VARCHAR(60) NOT NULL,
-  uf                CHAR(2) NOT NULL,
-  ponto_referencia  VARCHAR(100) NULL,
-  observacao        VARCHAR(MAX) NULL,
-  caminho         VARCHAR(120) NOT NULL,
-  data_envio      DATETIME NOT NULL,
-  status_req        INTEGER NULL,
+  id_requerimeto    INTEGER       NOT NULL IDENTITY(1,1),
+  id_cidadao        INTEGER       NOT NULL,
+  cod_controle      VARCHAR(10)   NOT NULL,
+  latitude          VARCHAR(50)   NULL,
+  longitude         VARCHAR(50)   NULL,
+  cep               INTEGER       NULL,
+  logradouro        VARCHAR(100)  NOT NULL,
+  numero            INTEGER       NULL, 
+  bairro            VARCHAR(60)   NOT NULL,
+  complemento       VARCHAR(60)   NULL,
+  cidade            VARCHAR(60)   NOT NULL,
+  uf                CHAR(2)       NOT NULL,
+  ponto_referencia  VARCHAR(100)  NULL,
+  observacao        VARCHAR(MAX)  NULL,
+  caminho         VARCHAR(120)    NOT NULL,
+  data_envio      DATETIME        NOT NULL,
+  status_req        INTEGER       NOT NULL,
   CONSTRAINT pk_tb_requerimento_id_requerimento PRIMARY KEY(id_requerimeto),
   CONSTRAINT fk_tb_requerimento_idcidadao FOREIGN KEY(id_cidadao)
     REFERENCES tb_cidadao(id_cidadao)
 );
 GO
-
-
-
-
-
 
 --IF object_id('tb_anexo') <>  NULL
 --	DROP TABLE tb_anexo
